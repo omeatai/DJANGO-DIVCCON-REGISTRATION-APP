1a2acc34c73d981414e00b2252a0c05423ae57fb
IFEANYI OMEATA <omeatai@gmail.com>
2022-05-05 16:23:25 +0100

    updates
    updates

--- divccon/urls.py
+++ divccon/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -13,6 +13,5 @@ urlpatterns = [
     path('registration_seven/', views.PhotoCreateView.as_view(), name='registration_seven'),
 ]
 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
